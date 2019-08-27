358587d2c72e7b66e003c0be96ba54b670c1af38
Munieru <20086673+munierujp@users.noreply.github.com>
2019-08-27 13:03:04 +0900

    Add sample records

--- init/3-insert_records.sql
+++ init/3-insert_records.sql
@@ -1,8 +1,10 @@
 -- users
 INSERT INTO `users` (`id`, `name`, `email`) VALUES
-(1, 'ムニエル', 'mail@munieru.jp');
+(1, 'ムニエル', 'mail@munieru.jp'),
+(2, '田中太郎', 'tanaka@example.com');
 
 -- trophies
 INSERT INTO `trophies` (`id`, `title`, `description`, `user_id`) VALUES
 (1, '誕生', 'この世に生を受けた。', 1),
-(2, 'My Trophy作成', 'My Trophyを作った。', 1);
+(2, 'My Trophy作成', 'My Trophyを作った。', 1),
+(3, 'テスト', 'トロフィー作成のテスト。', 2);
